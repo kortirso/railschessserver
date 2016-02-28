a36b494985b4fe4da6edfdbb96dda414c8e02705
kortirso <kortirso@gmail.com>
2016-02-28 22:08:18 +0300

    fixes

--- app/models/game.rb
+++ app/models/game.rb
@@ -23,11 +23,7 @@ class Game < ActiveRecord::Base
         result = from == to ? 'Должны быть указаны разные ячейки' : nil
         return result unless result.nil?
         errors = 0
-        [from, to].each do |index|
-            errors += 1 if %w(a b c d e f g h).index(index[0]).nil?
-            errors += 1 if %w(1 2 3 4 5 6 7 8).index(index[1]).nil?
-            errors += 1 if index.size > 2
-        end
+        [from, to].each { |index| errors += 1 if index.size > 2 || %w(a b c d e f g h).index(index[0]).nil? || %w(1 2 3 4 5 6 7 8).index(index[1]).nil? }
         result = errors > 0 ? 'Указана неправильная ячейка' : nil
     end
 
@@ -42,12 +38,10 @@ class Game < ActiveRecord::Base
     def check_turn(from, to)
         figure = self.board.cells.find_by(x_param: from[0], y_param: from[1]).figure
         finish_cell = self.board.cells.find_by(x_param: to[0], y_param: to[1]).figure
-        x_params = %w(a b c d e f g h)
-        y_params = %w(1 2 3 4 5 6 7 8)
+        x_params, y_params = %w(a b c d e f g h), %w(1 2 3 4 5 6 7 8)
         x_change = x_params.index(to[0]) - x_params.index(from[0])
         y_change = y_params.index(to[1]) - y_params.index(from[1])
-        p_pass = nil
-        roque = nil
+        p_pass, roque = nil, nil
         case figure.type
             when 'k'
                 result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход королем'
@@ -66,14 +60,10 @@ class Game < ActiveRecord::Base
                     result = result.nil? && did_k_turn && did_r_turn ? 'Нельзя рокироваться, фигуры двигались' : nil
                     roque = roque_figure if result.nil?
                 end
-            when 'q'
-                result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход ферзём'
-            when 'r'
-                result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход ладьёй'
-            when 'n'
-                result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход конём'
-            when 'b'
-                result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход слоном'
+            when 'q' then result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход ферзём'
+            when 'r' then result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход ладьёй'
+            when 'n' then result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход конём'
+            when 'b' then result = figure.beaten_fields.include?(to) ? nil : 'Неправильный ход слоном'
             when 'p'
                 near_x_param = x_params[x_params.index(from[0]) + x_change]
                 near_figure = self.board.cells.find_by(x_param: near_x_param, y_param: from[1]).figure
@@ -81,7 +71,6 @@ class Game < ActiveRecord::Base
                     last_turn = self.turns.last
                     p_pass = near_figure if last_turn.to == "#{near_x_param}#{from[1]}" && last_turn.from == "#{near_x_param}#{from[1].to_i + y_change * 2}"
                 end
-
                 result = figure.color == 'white' && (x_change == 0 && y_change == 1 && finish_cell.nil? || x_change == 0 && y_change == 2 && from[1] == '2' && finish_cell.nil? || figure.beaten_fields.include?(to) && !finish_cell.nil? && finish_cell.color == 'black' || figure.beaten_fields.include?(to) && !p_pass.nil?) || figure.color == 'black' && (x_change == 0 && y_change == -1 && finish_cell.nil? || x_change == 0 && y_change == -2 && from[1] == '7' && finish_cell.nil? || figure.beaten_fields.include?(to) && !finish_cell.nil? && finish_cell.color == 'white' || figure.beaten_fields.include?(to) && !p_pass.nil?) ? nil : 'Неправильный ход пешкой'
         end
         return result unless result.nil?
