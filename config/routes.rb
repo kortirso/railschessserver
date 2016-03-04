5209672182a14b4cd12b1b35ac1879a6ad24758a
kortirso <kortirso@gmail.com>
2016-03-04 08:37:40 +0300

    add welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
     devise_for :users
-    resources :games, except: [:edit, :update]
     
+    resources :games, except: [:edit, :update]
     namespace :chess do
         post 'turn' => 'turn#index', as: 'make_turn'
         get 'surrender/:game/:user' => 'surrender#index', as: 'surrender'
     end
 
-    root to: 'games#index'
+    root to: 'welcome#index'
 end
---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,3 @@
+RSpec.describe WelcomeController, type: :controller do
+
+end
