4618b6428a042aea95cf1ab690a69c24e9ea08ff
kortirso <kortirso@gmail.com>
2016-03-06 19:50:49 +0300

    fix

--- app/controllers/ai/start_controller.rb
+++ app/controllers/ai/start_controller.rb
@@ -3,7 +3,7 @@ class Ai::StartController < ApplicationController
         if session[:guest].nil?
             render nothing: true
         else
-            Game.find_by(guest: session[:guest]).destroy
+            Game.where(guest: session[:guest]).destroy_all
             game = Game.build(0, User.find_by(username: 'Коала Майк').id, false, 0, session[:guest])
             redirect_to game
         end
