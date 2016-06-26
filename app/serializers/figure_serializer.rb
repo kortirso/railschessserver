f8035d3912b8e530006b1a7143d7ae13f0d0db26
kortirso <kortirso@gmail.com>
2016-06-26 20:13:13 +0300

    fix serializer bug

--- app/serializers/figure_serializer.rb
+++ app/serializers/figure_serializer.rb
@@ -2,6 +2,6 @@ class FigureSerializer < ActiveModel::Serializer
     attributes :id, :color, :type, :cell_name, :beaten_fields
 
     def cell_name
-        object.cell.name
+        object.cell.nil? ? nil : object.cell.name
     end
 end
