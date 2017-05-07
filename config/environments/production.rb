5323edda75d4aaf89fe5aee6aae505bd728a8d96
kortirso <kortirso@gmail.com>
2017-05-07 11:15:53 +0300

    update for ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,4 +12,5 @@ Rails.application.configure do
     config.active_support.deprecation = :notify
     config.log_formatter = ::Logger::Formatter.new
     config.active_record.dump_schema_after_migration = false
+    config.force_ssl = true
 end
